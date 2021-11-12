4b6ef1525c9b176ef57f4fc798e311ea36c63206
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-12 10:16:47 -0700

    prompts for building profile

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,7 +3,7 @@ const Intern = require('../lib/Intern');
 test('creates an intern object', () => {
     const intern = new Intern('Bob');
 
-    expect(intern.name).toBe('John');
+    expect(intern.name).toBe('Bob');
     expect(intern.school).toEqual(expect.stringContaining(intern.school.toString()));
     
 })
--- index.js
+++ index.js
@@ -1,8 +1,81 @@
-function generateProfile() {
+const { generate } = require('escodegen');
+const inquirer = require('inquirer');
+
+const generateManager = () => {
+    console.log(`Let's build your team profile!`)
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is your manager's name?"
+            },
+            {
+                type: 'input',
+                name: 'managerID',
+                message: "What is your manager's ID?"
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is your manager's email?"
+            },
+            {
+                type: 'input',
+                name: 'managerOffice',
+                message: "What is your manager's office number?"
+            }
+        ])
+
 
 }
 
-generateProfile.prototype.initializeQuestions = function () {
-    inquirer
-        .prompt
+const generateEmployee = () => {
+    console.log(`Let's add some employees!`)
+    return inquirer
+        .prompt(
+            {
+                type: 'input',
+                name: 'employeeName',
+                message: "What is the employee name?"
+            },
+            {
+                type: 'input',
+                name: 'employeeId',
+                message: "What is the employee's ID?"
+            },
+            {
+                type: 'list',
+                name: 'employeeRole',
+                message: "What is the employee's role?",
+                choices: ['Engineer', 'Intern']
+            })
+        .then(({ employeeRole }) => {
+            if (employeeRole === 'Intern') {
+                return inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            name: 'internSchool',
+                            message: "What is the intern's school?"
+                        }
+                    )
+            } else {
+                if (employeeRole === 'Engineer') {
+                    return inquirer
+                        .prompt(
+                            {
+                                type: 'input',
+                                name: 'engineerGithub',
+                                message: "What is the engineer's github?"
+                            }
+                        )
+                }
+            }
+        })
+
 }
+
+
+generateManager()
+    .then(generateEmployee)

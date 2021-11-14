0c45ef31e2c944f08fbaf8f4ae79648622a03784
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-14 13:45:47 -0700

    manager test working

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,12 @@
 const Manager = require('../lib/Manager');
 
 test('creates a Manager Object', () => {
-    const manager = new Manager('Steve');
+    const manager = new Manager('Steve', '3', '1');
 
 
-    expect(manager.name).toBe('Steve');
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+    
+    expect(manager.getOfficeNumber()).toEqual(manager.managerOffice);
+    expect(manager.getID()).toEqual(manager.managerID);
+    expect(manager.getRole()).toBe(Manager);
 
 })
